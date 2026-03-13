f82cfffd1cf3bbb865a5d94db4b35ceb0a24abc4
Oleg <oskolkov.web@gmail.com>
2026-03-13 23:59:41 +0300

    ff

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -4,15 +4,19 @@ import numpy as np
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
     if pad_size < 1:
         raise ValueError
-        
+
     new_sh = list(image.shape)
     new_sh[0] += 2 * pad_size
     new_sh[1] += 2 * pad_size
-    
+
     with_pad = np.zeros(new_sh, dtype=image.dtype)
-    
-    with_pad[pad_size:-pad_size, pad_size:-pad_size,] = image
+
+    with_pad[
+        pad_size:-pad_size,
+        pad_size:-pad_size,
+    ] = image
     return with_pad
+
 
 def blur_image(
     image: np.ndarray,
@@ -20,28 +24,47 @@ def blur_image(
 ) -> np.ndarray:
     if kernel_size < 1 or kernel_size % 2 == 0:
         raise ValueError
-        
+
     if kernel_size == 1:
         return image
-        
+
     pad_size = kernel_size // 2
     with_pad = pad_image(image, pad_size)
     h, w = image.shape[:2]
-    
+
     csy = np.cumsum(with_pad, axis=0)
     y_sum = np.zeros((h, with_pad.shape[1]) + image.shape[2:])
-    
+
     y_sum[0,] = csy[kernel_size - 1,]
     y_sum[1:,] = csy[kernel_size:,] - csy[:-kernel_size,]
-    
+
     csx = np.cumsum(y_sum, axis=1)
     xy_sum = np.zeros((h, w) + image.shape[2:])
-    
-    xy_sum[:, 0,] = csx[:, kernel_size - 1,]
-    xy_sum[:, 1:,] = csx[:, kernel_size:,] - csx[:, :-kernel_size,]
-    
-    blur_im = xy_sum / (kernel_size ** 2)
+
+    xy_sum[
+        :,
+        0,
+    ] = csx[
+        :,
+        kernel_size - 1,
+    ]
+    xy_sum[
+        :,
+        1:,
+    ] = (
+        csx[
+            :,
+            kernel_size:,
+        ]
+        - csx[
+            :,
+            :-kernel_size,
+        ]
+    )
+
+    blur_im = xy_sum / (kernel_size**2)
     return blur_im.astype(image.dtype)
+
 
 if __name__ == "__main__":
     import os
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,30 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    flat = image.flatten()
+
+    counts = np.zeros(256, dtype=np.int64)
+    colors = np.arange(256, dtype=np.int32).reshape(-1, 1)
+
+    np.add.at(counts, flat, 1)
+
+    valid_colors = counts > 0
+
+    diffs = np.abs(colors - colors.T)
+    mask = diffs < threshold
+
+    groups = np.sum(mask * counts, axis=1)
+
+    scores = groups * (image.size + 1) + counts
+
+    scores[~valid_colors] = -1
+
+    ans_ind = np.argmax(scores)
+
+    ans = np.uint8(ans_ind)
+    percent = float(groups[ans_ind] / image.size)
+
+    return ans, percent
